1102c185969efeb8568be580be6a703afd96cf14
ravishankar0908 <ravishankar007cbi@gmail.com>
2025-06-05 15:57:13 +0530

    a alluser middleware is created and small bug fixed in the refresh token

---
+++ middlewares/alluserMiddleware.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import { messages, statusCodes } from "../util/responseStatuscodes.js";
+export const alluserAuthourization = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  const { userId } = req.query;
+
+  if (token == null)
+    return res.status(statusCodes.notFound).json({
+      message: messages.invalidToken,
+    });
+
+  jwt.verify(token, process.env.jwt_secret_key, (err, user) => {
+    if (err)
+      return res.status(statusCodes.notFound).json({ message: err.message });
+
+    if (user.role === "user" && user.id !== userId) {
+      return res.status(statusCodes.unauth).json({
+        message: messages.unauthorized,
+      });
+    }
+    req.user = user;
+    next();
+  });
+};
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { allUserList, userById } from "../controllers/userController.js";
 import { userAuthourization } from "../middlewares/userMiddleware.js";
+import { alluserAuthourization } from "../middlewares/alluserMiddleware.js";
 const router = express.Router();
 
-router.get("/", userAuthourization, allUserList);
-router.get("/userbyid", userAuthourization, userById);
+router.get("/", alluserAuthourization, allUserList);
+router.get("/userbyid", alluserAuthourization, userById);
 
 export default router;
