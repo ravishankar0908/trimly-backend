e0838f492fd7a2dbb75ee42ed83bfd48a8641536
ravishankar0908 <ravishankar007cbi@gmail.com>
2025-07-09 21:07:31 +0530

    stylist's specialization is implemented

--- routers/stylistRouter.js
+++ routers/stylistRouter.js
@@ -1,9 +1,13 @@
-import { insertSpecialization } from "../controllers/stylistController.js";
+import {
+  getAllSpecialization,
+  insertSpecialization,
+} from "../controllers/stylistController.js";
 import { userAuthourization } from "../middlewares/userMiddleware.js";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/add", userAuthourization, insertSpecialization);
+router.get("/", userAuthourization, getAllSpecialization);
 
 export default router;
